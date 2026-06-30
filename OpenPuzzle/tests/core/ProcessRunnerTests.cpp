8727b185131853df6e514b13d74ed1cde83504e8
claudiodb007 <geral@claudiodb.com>
2026-06-30 12:24:00 +0100

    Add ProcessRunner test

---
+++ OpenPuzzle/tests/core/ProcessRunnerTests.cpp
@@ -0,0 +1,27 @@
+#include "openpuzzle/core/ProcessRunner.hpp"
+
+using namespace openpuzzle;
+
+int main() {
+    ProcessRunner runner;
+
+    int lines = 0;
+
+    auto result = runner.run("printf 'alpha\\nbeta\\n'", [&](const std::string&) {
+        lines++;
+    });
+
+    if (!result.started) {
+        return 1;
+    }
+
+    if (result.exitCode != 0) {
+        return 2;
+    }
+
+    if (lines != 2) {
+        return 3;
+    }
+
+    return 0;
+}
