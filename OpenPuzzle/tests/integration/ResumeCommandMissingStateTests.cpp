54097a661c43fa468208c461965eb8d18a9703a7
claudiodb007 <geral@claudiodb.com>
2026-06-30 19:51:32 +0100

    Add resume latest job support

--- OpenPuzzle/tests/integration/ResumeCommandMissingStateTests.cpp
+++ OpenPuzzle/tests/integration/ResumeCommandMissingStateTests.cpp
@@ -1,19 +1,20 @@
 #include <cstdlib>
 #include <filesystem>
 
-int main()
-{
-    auto temp = std::filesystem::temp_directory_path() / "openpuzzle_resume_missing_state_test";
-    std::filesystem::remove_all(temp);
+int main() {
+  auto temp = std::filesystem::temp_directory_path() /
+              "openpuzzle_resume_missing_state_test";
+  std::filesystem::remove_all(temp);
 
-    std::filesystem::create_directories(temp / "jobs" / "00000042");
+  std::filesystem::create_directories(temp / "jobs" / "00000042");
 
-    std::string command =
-        "./OpenPuzzle resume --job 42 --base " + temp.string() + " > /tmp/openpuzzle_resume_missing_stdout.txt 2> /tmp/openpuzzle_resume_missing_stderr.txt";
+  std::string command = "./OpenPuzzle resume --job 42 --base " + temp.string() +
+                        " > /tmp/openpuzzle_resume_missing_stdout.txt 2> "
+                        "/tmp/openpuzzle_resume_missing_stderr.txt";
 
-    int code = std::system(command.c_str());
+  int code = std::system(command.c_str());
 
-    std::filesystem::remove_all(temp);
+  std::filesystem::remove_all(temp);
 
-    return code != 0 ? 0 : 1;
+  return code != 0 ? 0 : 1;
 }
