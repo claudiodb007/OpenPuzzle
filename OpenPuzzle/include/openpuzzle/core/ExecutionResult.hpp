c3d497cfac77917b6411b615d601db6ac3aba437
claudiodb007 <geral@claudiodb.com>
2026-06-30 11:25:47 +0100

    Add ExecutionResult model

---
+++ OpenPuzzle/include/openpuzzle/core/ExecutionResult.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace openpuzzle {
+
+struct ExecutionResult {
+
+    bool success = false;
+
+    int exitCode = -1;
+
+    std::uint64_t linesRead = 0;
+
+    double averageSpeed = 0.0;
+
+    bool keyFound = false;
+
+    std::string privateKey;
+};
+
+} // namespace openpuzzle
